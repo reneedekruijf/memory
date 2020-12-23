4fcf2bfc94a7e17e8e4d864915256c9cef658b87
Renee <renee@webdesignpuntnl.com>
2020-12-23 19:02:25 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const boxes = document.querySelectorAll('.card');
+
+boxes.forEach(box => box.addEventListener('click', (e) => {
+  box.classList.toggle('is-flipped')
+}));
+
+// input with name player one
+// input with name player one
+// button to start the game
+
+// array with pictures
+// 6 times 2 the same pictures
+// fill card random with pictures
+
